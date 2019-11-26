c399161407c647e5e1b4b247a4d9785cd74c04b4
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-11-26 11:13:31 -0500

    completed file_io.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,7 +8,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+f = open('foo.txt', 'r')
+
+print(f.read())
+
+f.close()
+
+print(f.closed)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +22,12 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar = open('bar.txt', 'w+')
+
+bar.write("Someone said left they left together\n"
+          "I ran out the door to get her\n"
+          "Oh my love, can't you see yourself by my side?")
+
+bar = open('bar.txt', 'r')
+
+print(bar.read())
