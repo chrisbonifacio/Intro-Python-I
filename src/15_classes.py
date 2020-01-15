6c5e92271982fd5d5a727a3a2faaa33a472b6545
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-01-15 01:22:05 -0500

    finished 15_classes.py

--- src/15_classes.py
+++ src/15_classes.py
@@ -10,53 +10,54 @@ class LatLon:
         self.lon = lon
 
 
-coordinates = LatLon(13, 25)
-
-print(coordinates.lat)
-print(coordinates.lon)
-
-
-def attack(name, power):
-    print(f"{name}: {power}")
-
-
-class Hero:
-    def __init__(self, hp_stat,
-                 atk_stat, spd_stat,
-                 def_stat, res_stat):
-        self.hp_stat = hp_stat
-        self.atk_stat = atk_stat
-        self.spd_stat = spd_stat
-        self.def_stat = def_stat
-        self.res_stat = res_stat
-
-
-roy = Hero(24, 16, 12, 10, 7)
-
-attack('fire emblem', 25)
-
-
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+
+
+class Waypoint(LatLon):
+    def __init__(self, name, lat, lon):
+        self.name = name
+        super().__init__(lat, lon)
+
+    def __str__(self):
+        return "{} {}, {}".format(self.name, self.lat, self.lon)
+
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
 
+
+class Geocache(Waypoint):
+    def __init__(self, name, difficulty, size, lat, lon):
+        self.difficulty = difficulty
+        self.size = size
+        super().__init__(name, lat, lon)
+
+    def __str__(self):
+        return "{}, diff {}, size {}, {}, {}".format(self.name, self.difficulty, self.size, self.lat, self.lon)
+
+
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
+waypoint = Waypoint("Catacombs", 41.70505, -121.51521)
+
+print(waypoint)
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-#print(waypoint)
+# print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
+geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
+
+print(geocache)
 
 # Print it--also make this print more nicely
-#print(geocache)
+# print(geocache)
